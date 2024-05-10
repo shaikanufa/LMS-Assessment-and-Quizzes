a56260679a4f5415784e1d7a73515ab10577cea8
shaikanufa <164306750+shaikanufa@users.noreply.github.com>
2024-05-10 17:04:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AssignmentCreation from './compnents/AssignmentCreation';
+import './App.css';
+import './index.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+       
+      <AssignmentCreation/>
+  
+    </div>
+  );
+}
+
+export default App;
